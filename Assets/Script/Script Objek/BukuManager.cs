2184ffa7c4bdc4fa8c4bd745b0572ee6e367c276
AkuFauzi <fauziabdillah009@gmail.com>
2023-11-27 21:53:13 +0700

    up

--- Assets/Script/Script Objek/BukuManager.cs
+++ Assets/Script/Script Objek/BukuManager.cs
@@ -13,7 +13,7 @@ public class BukuManager : MonoBehaviourPun
     public GameObject[] Lembar;
 
 
-    bool IsOpen;
+    public bool IsOpen;
 
     // Start is called before the first frame update
     void Start()
--- Assets/Script/Script Objek/ButtonManager.cs
+++ Assets/Script/Script Objek/ButtonManager.cs
@@ -1,18 +1,41 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonManager : MonoBehaviour
+public class ButtonManager : MonoBehaviourPun
 {
+    public BukuManager BukuManager;
+
+    bool IsNext;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BukuManager = GameObject.FindObjectOfType<BukuManager>().GetComponent<BukuManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void OnClickNext()
+    {
+        BukuManager.Lembar[1].SetActive(false);
+        BukuManager.Lembar[2].SetActive(false);
+        BukuManager.Lembar[0].SetActive(true);
+    }
+
+    public void OnClickPrev()
+    {
+        BukuManager.Lembar[1].SetActive(true);
+        BukuManager.Lembar[2].SetActive(true);
+        BukuManager.Lembar[0].SetActive(false);
+    }
+
+    public void OnClickExitBook()
+    {
+        BukuManager.buku.SetActive(false);
+        BukuManager.IsOpen = false;
     }
 }
